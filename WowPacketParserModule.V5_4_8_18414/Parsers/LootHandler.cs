d941512292e617f04500b80b2a95e5480c085431
ChipLeo <winleo@ua.fm>
2014-09-24 04:23:51 +0300

    Added CMSG_LOOT

--- WowPacketParserModule.V5_4_8_18414/Parsers/LootHandler.cs
+++ WowPacketParserModule.V5_4_8_18414/Parsers/LootHandler.cs
@@ -14,7 +14,9 @@ namespace WowPacketParserModule.V5_4_8_18414.Parsers
         [Parser(Opcode.CMSG_LOOT)]
         public static void HandleLoot(Packet packet)
         {
-            packet.ReadToEnd();
+            var guid = packet.StartBitStream(4, 5, 2, 7, 0, 1, 3, 6);
+            packet.ParseBitStream(guid, 3, 5, 0, 6, 4, 1, 7, 2);
+            packet.WriteGuid("Guid", guid);
         }
 
         [Parser(Opcode.CMSG_LOOT_MASTER_GIVE)]
