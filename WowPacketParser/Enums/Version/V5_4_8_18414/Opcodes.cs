948caa9caeba8609c382b55e8a1b56658a4bda89
ChipLeo <winleo@ua.fm>
2014-11-19 12:22:28 +0200

    Added SMSG_ADDON_INFO

--- WowPacketParser/Enums/Version/V5_4_8_18414/Opcodes.cs
+++ WowPacketParser/Enums/Version/V5_4_8_18414/Opcodes.cs
@@ -439,7 +439,7 @@ namespace WowPacketParser.Enums.Version.V5_4_8_18414
             {Opcode.SMSG_ACTION_BUTTONS,                           0x081A | 0x20000}, //++
             {Opcode.SMSG_ACTIVATETAXIREPLY,                        0x02A7 | 0x20000}, //++
             {Opcode.SMSG_ADD_LOSS_OF_CONTROL,                      0x0C13 | 0x20000}, //++
-            {Opcode.SMSG_ADDON_INFO,                               0x160A | 0x20000}, //--
+            {Opcode.SMSG_ADDON_INFO,                               0x160A | 0x20000}, //++
             {Opcode.SMSG_ADJUST_SPLINE_DURATION,                   0x023B | 0x20000}, //++
             {Opcode.SMSG_AI_REACTION,                              0x06AF | 0x20000}, //++
             {Opcode.SMSG_ALL_ACHIEVEMENT_DATA_ACCOUNT,             0x0A9E | 0x20000}, //++
