c87fa2055fb3ce45570edd070467b20f87f78da1
Aaron126 <world_of_warcraft@ymail.com>
2015-03-08 16:05:22 -0500

    Added 64 opcodes for 6.1.2
    I'll do SMSGs tomorrow, sleep now T_T

--- WowPacketParser/Enums/Version/V6_1_2_19728/Opcodes.cs
+++ WowPacketParser/Enums/Version/V6_1_2_19728/Opcodes.cs
@@ -21,6 +21,8 @@ namespace WowPacketParser.Enums.Version.V6_1_2_19728
 
         private static readonly BiDictionary<Opcode, int> ClientOpcodes = new BiDictionary<Opcode, int>
         {
+            {Opcode.CMSG_ADD_FRIEND, 0x039E},
+            {Opcode.CMSG_ADD_IGNORE, 0x16A0},
             {Opcode.CMSG_ATTACKSTOP, 0x0A01},
             {Opcode.CMSG_ATTACKSWING, 0x0BF4},
             {Opcode.CMSG_AUTH_CONTINUED_SESSION, 0x0485},
@@ -30,23 +32,51 @@ namespace WowPacketParser.Enums.Version.V6_1_2_19728
             {Opcode.CMSG_BATTLEFIELD_JOIN_RATED, 0x01AA},             // Unconfirmed
             {Opcode.CMSG_BATTLEFIELD_LEAVE, 0x0272},                  // Unconfirmed
             {Opcode.CMSG_BATTLEFIELD_PORT, 0x1D32},                   // Unconfirmed
+            {Opcode.CMSG_BUY_BACK_ITEM, 0x1E84},
             {Opcode.CMSG_BUY_BANK_SLOT, 0x1DE2},
+            {Opcode.CMSG_BUY_ITEM, 0x1255},
+            {Opcode.CMSG_BUY_ITEM, 0x1931},
             {Opcode.CMSG_BUY_ITEM, 0x1CE5},
             {Opcode.CMSG_CANCEL_AURA, 0x12FB},
             {Opcode.CMSG_CANCEL_CAST, 0x058A},
+            {Opcode.CMSG_CANCEL_TRADE, 0x0E0F},                       // Unconfirmed
             {Opcode.CMSG_CAST_SPELL, 0x1274},
             {Opcode.CMSG_CHANGE_SUB_GROUP, 0x0AB7},                   // Unconfirmed
+            {Opcode.CMSG_CHANNEL_BAN, 0x187E},
+            {Opcode.CMSG_CHANNEL_INVITE, 0x142D},
+            {Opcode.CMSG_CHANNEL_KICK, 0x153D},
+            {Opcode.CMSG_CHANNEL_MODERATOR, 0x152D},
+            {Opcode.CMSG_CHANNEL_MUTE, 0x1909},
+            {Opcode.CMSG_CHANNEL_OWNER, 0x14CA},
+            {Opcode.CMSG_CHANNEL_PASSWORD, 0x193A},
+            {Opcode.CMSG_CHANNEL_UNBAN, 0x155D},
+            {Opcode.CMSG_CHANNEL_UNMODERATOR, 0x10AE},
+            {Opcode.CMSG_CHANNEL_UNMUTE, 0x109A},
             {Opcode.CMSG_CHAR_CREATE, 0x1636},
             {Opcode.CMSG_CHAR_DELETE, 0x12B8},
             {Opcode.CMSG_CHAR_ENUM, 0x1696},
+            {Opcode.CMSG_CLOSE_INTERACTION, 0x1C36},
             {Opcode.CMSG_CONVERT_RAID, 0x0A98},                       // Unconfirmed
+            {Opcode.CMSG_CREATURE_QUERY, 0x0FD3},
+            {Opcode.CMSG_DB_QUERY_BULK, 0x0836},
+            {Opcode.CMSG_DEL_FRIEND, 0x163F},
+            {Opcode.CMSG_DEL_IGNORE, 0x03C0},
+            {Opcode.CMSG_DELETE_EQUIPMENT_SET, 0x014A},
             {Opcode.CMSG_DEPOSIT_REAGENT_BANK, 0x002A},
             {Opcode.CMSG_DO_READY_CHECK, 0x139E},                     // Unconfirmed
             {Opcode.CMSG_DUEL_PROPOSED, 0x0A38},
             {Opcode.CMSG_DUEL_RESPONSE, 0x0A5B},                      // Unconfirmed
             {Opcode.CMSG_EMOTE, 0x01AD},
+            {Opcode.CMSG_EQUIPMENT_SET_SAVE, 0x09E2},
+            {Opcode.CMSG_GAMEOBJECT_QUERY, 0x06C8},
+            {Opcode.CMSG_GARRISON_MISSION_BONUS_ROLL, 0x07D2},        // Unconfirmed
+            {Opcode.CMSG_GET_UNDELETE_COOLDOWN_STATUS, 0x063D},
+            {Opcode.CMSG_GOSSIP_SELECT_OPTION, 0x1E0C},               // Unconfirmed
+            {Opcode.CMSG_GUILD_BANK_BUY_TAB, 0x0F09},
             {Opcode.CMSG_GUILD_BANK_DEPOSIT_MONEY, 0x0832},           // Unconfirmed
             {Opcode.CMSG_GUILD_DECLINE_INVITATION, 0x1967},           // Unconfirmed
+            {Opcode.CMSG_GUILD_QUERY, 0x12BE},
+            {Opcode.CMSG_GUILD_QUERY, 0x1914},
             {Opcode.CMSG_JOIN_ARENA, 0x0865},                         // Unconfirmed
             {Opcode.CMSG_JOIN_ARENA_SKIRMISH, 0x1E01},
             {Opcode.CMSG_JOIN_CHANNEL, 0x152A},
@@ -58,6 +88,7 @@ namespace WowPacketParser.Enums.Version.V6_1_2_19728
             {Opcode.CMSG_LFG_SET_COMMENT, 0x0615},                    // Unconfirmed
             {Opcode.CMSG_LIST_INVENTORY, 0x1922},
             {Opcode.CMSG_LOAD_SCREEN, 0x13C0},
+            {Opcode.CMSG_LOG_DISCONNECT, 0x12D5},
             {Opcode.CMSG_LOGOUT_CANCEL, 0x0F8C},
             {Opcode.CMSG_LOGOUT_REQUEST, 0x0CA6},
             {Opcode.CMSG_LOOT_METHOD, 0x0E3E},                        // Unconfirmed
@@ -70,6 +101,7 @@ namespace WowPacketParser.Enums.Version.V6_1_2_19728
             {Opcode.CMSG_MESSAGECHAT_SAY, 0x192A},
             {Opcode.CMSG_MESSAGECHAT_WHISPER, 0x103A},
             {Opcode.CMSG_MESSAGECHAT_YELL, 0x1CB9},
+            {Opcode.CMSG_MOUNT_SET_FAVORITE, 0x061E},
             {Opcode.CMSG_MOVE_FALL_LAND, 0x095F},
             {Opcode.CMSG_MOVE_HEARTBEAT, 0x055C},
             {Opcode.CMSG_MOVE_JUMP, 0x0158},
@@ -81,10 +113,15 @@ namespace WowPacketParser.Enums.Version.V6_1_2_19728
             {Opcode.CMSG_MOVE_START_FORWARD, 0x0004},
             {Opcode.CMSG_MOVE_START_STRAFE_LEFT, 0x0844},
             {Opcode.CMSG_MOVE_START_STRAFE_RIGHT, 0x0957},
+            {Opcode.CMSG_MOVE_START_TURN_LEFT, 0x0918},
+            {Opcode.CMSG_MOVE_START_TURN_RIGHT, 0x094B},
             {Opcode.CMSG_MOVE_STOP, 0x044B},
             {Opcode.CMSG_MOVE_STOP_ASCEND, 0x011C},
             {Opcode.CMSG_MOVE_STOP_STRAFE, 0x084B},
+            {Opcode.CMSG_MOVE_STOP_TURN, 0x0854},
             {Opcode.CMSG_NAME_QUERY, 0x0BBD},
+            {Opcode.CMSG_NPC_TEXT_QUERY, 0x1E24},
+            {Opcode.CMSG_OBJECT_UPDATE_RESCUED, 0x0DA5},
             {Opcode.CMSG_OPT_OUT_OF_LOOT, 0x1F89},                    // Unconfirmed
             {Opcode.CMSG_PARTY_INVITE, 0x12BD},
             {Opcode.CMSG_PARTY_INVITE_RESPONSE, 0x16BF},              // Unconfirmed
@@ -96,42 +133,69 @@ namespace WowPacketParser.Enums.Version.V6_1_2_19728
             {Opcode.CMSG_PET_SPELL_AUTOCAST, 0x0C75},                 // Unconfirmed
             {Opcode.CMSG_PET_STOP_ATTACK, 0x09A6},                    // Unconfirmed
             {Opcode.CMSG_PING, 0x12DE},
+            {Opcode.CMSG_PLAYED_TIME, 0x0750},
             {Opcode.CMSG_PLAYER_LOGIN, 0x0E98},
             {Opcode.CMSG_PORT_GRAVEYARD, 0x0C65},                     // Unconfirmed
+            {Opcode.CMSG_QUEUED_MESSAGES_END, 0x027E},
+            {Opcode.CMSG_RANDOMIZE_CHAR_NAME, 0x0B3E},
             {Opcode.CMSG_READY_CHECK_RESPONSE, 0x07B5},               // Unconfirmed
             {Opcode.CMSG_REAGENT_BANK_BUY_TAB, 0x1D75},
+            {Opcode.CMSG_REORDER_CHARACTERS, 0x17B7},
             {Opcode.CMSG_REQUEST_ACCOUNT_DATA, 0x0798},
             {Opcode.CMSG_REQUEST_PVP_OPTIONS_ENABLED, 0x029E},        // Unconfirmed
             {Opcode.CMSG_REQUEST_PVP_REWARDS, 0x06DC},                // Unconfirmed
             {Opcode.CMSG_REQUEST_RAID_INFO, 0x0A96},                  // Unconfirmed
+            {Opcode.CMSG_REQUEST_RATED_INFO, 0x0A40},                 // Unconfirmed
+            {Opcode.CMSG_ROUTER_CLIENT_LOG_STREAMING_ERROR, 0x12D6},
+            {Opcode.CMSG_SAVE_CUF_PROFILES, 0x0EC7},
             {Opcode.CMSG_SET_ACHIEVEMENTS_HIDDEN, 0x16D0},
+            {Opcode.CMSG_SET_ACTION_BUTTON, 0x133F},
+            {Opcode.CMSG_SET_ACTIVE_MOVER, 0x0108},
             {Opcode.CMSG_SET_ASSISTANT_LEADER, 0x0395},               // Unconfirmed
+            {Opcode.CMSG_SET_CONTACT_NOTES, 0x0B3D},
+            {Opcode.CMSG_SET_DUNGEON_DIFFICULTY, 0x0E16},             // Unconfirmed
             {Opcode.CMSG_SET_EVERYONE_IS_ASSISTANT, 0x1716},          // Unconfirmed
+            {Opcode.CMSG_SET_FACTION_AT_WAR, 0x1C66},                 // Unconfirmed
             {Opcode.CMSG_SET_FACTION_INACTIVE, 0x1862},
             {Opcode.CMSG_SET_PARTY_LEADER, 0x131D},                   // Unconfirmed
+            {Opcode.CMSG_SET_PVP, 0x1BC7},
+            {Opcode.CMSG_SET_RAID_DIFFICULTY, 0x0397},                // Unconfirmed
+            {Opcode.CMSG_SET_ROLE, 0x0398},
             {Opcode.CMSG_SET_SELECTION, 0x0E8C},
             {Opcode.CMSG_SET_SPECIALIZATION, 0x0759},
+            {Opcode.CMSG_SET_TITLE, 0x1650},
             {Opcode.CMSG_SET_WATCHED_FACTION, 0x1E82},
+            {Opcode.CMSG_SHOW_FRIENDS, 0x0EC0},
             {Opcode.CMSG_SHOWING_CLOAK, 0x0F04},
             {Opcode.CMSG_SHOWING_HELM, 0x0C36},
             {Opcode.CMSG_SORT_BAGS, 0x0AF1},
+            {Opcode.CMSG_SORT_BANK_BAGS, 0x659},                      // Unconfirmed
             {Opcode.CMSG_SORT_REAGENT_BANK_BAGS, 0x06D2},
+            {Opcode.CMSG_SPLIT_ITEM, 0x052B},
             {Opcode.CMSG_START_SPECTATOR_WAR_GAME, 0x16B5},           // Unconfirmed
             {Opcode.CMSG_START_WARGAME, 0x012BF},                     // Unconfirmed
+            {Opcode.CMSG_SUPPORT_TICKET_SUBMIT_BUG, 0x06B6},
+            {Opcode.CMSG_SUPPORT_TICKET_SUBMIT_COMPLAINT, 0x16C0},
+            {Opcode.CMSG_SUPPORT_TICKET_SUBMIT_SUGGESTION, 0x1A16},   // Unconfirmed
+            {Opcode.CMSG_SUSPEND_COMMS_ACK, 0x1375},
             {Opcode.CMSG_SWAP_INV_ITEM, 0x003C},
             {Opcode.CMSG_SWAP_ITEM, 0x0438},
             {Opcode.CMSG_SWAP_SUB_GROUPS, 0x0F98},                    // Unconfirmed
+            {Opcode.CMSG_TIME_SYNC_RESPONSE, 0x0550},
+            {Opcode.CMSG_TOGGLE_PVP, 0x1BAB},
             {Opcode.CMSG_TRANSMOGRIFY_ITEMS, 0x03F1},
             {Opcode.CMSG_UI_TIME_REQUEST, 0x0550},
             {Opcode.CMSG_UNLEARN_SPECIALIZATION, 0x0708},             // Unconfirmed
             {Opcode.CMSG_UNLEARN_TALENTS, 0x0FA9},
             {Opcode.CMSG_UPDATE_ACCOUNT_DATA, 0x1637},
+            {Opcode.CMSG_USE_EQUIPMENT_SET, 0x083C},
             {Opcode.CMSG_USE_ITEM, 0x06D0},
             {Opcode.CMSG_VIOLENCE_LEVEL, 0x0F48},
             {Opcode.CMSG_VOID_STORAGE_TRANSFER, 0x0E07},
             {Opcode.CMSG_VOID_STORAGE_UNLOCK, 0x0AA1},
+            {Opcode.CMSG_WARDEN_DATA, 0x02B8},
             {Opcode.CMSG_WARGAME_RESPONSE, 0x0E3F},                   // Unconfirmed
-            {Opcode.CMSG_REQUEST_RATED_INFO, 0x0A40},                 // Unconfirmed
+            {Opcode.CMSG_WHO, 0x079E},                                // Unconfirmed
         };
 
         private static readonly BiDictionary<Opcode, int> ServerOpcodes = new BiDictionary<Opcode, int>
