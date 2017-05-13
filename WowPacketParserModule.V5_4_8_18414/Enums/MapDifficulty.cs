1404382c7a04111260e7e5606b110d560dcc0892
ChipLeo <winleo@ua.fm>
2017-05-13 23:13:08 +0300

    Updated MapDifficulty 18414

--- WowPacketParserModule.V5_4_8_18414/Enums/MapDifficulty.cs
+++ WowPacketParserModule.V5_4_8_18414/Enums/MapDifficulty.cs
@@ -3,17 +3,17 @@ namespace WowPacketParserModule.V5_4_8_18414.Enums
     public enum MapDifficulty
     {
         NoneDifficulty          = 0,
-        DungeonNormal5man       = 1,
-        DungeonHeroic5Man       = 2,
-        RaidNormal10Man         = 3,
-        RaidNormal25Man         = 4,
-        RaidHeroic10Man         = 5,
-        RaidHeroic25Man         = 6,
-        RaidLfr25Man            = 7,
-        DungeonChallenge5Man    = 8,
-        RaidFlex10Man           = 9,
-        ScenarioHeroic          = 11,
-        ScenarioNormal          = 12,
-        RaidDynamicDiff         = 14
+        DungeonNormal5man       = 1,  // Обычный
+        DungeonHeroic5Man       = 2,  // Героический
+        RaidNormal10Man         = 3,  // 10 игроков
+        RaidNormal25Man         = 4,  // 25 игроков
+        RaidHeroic10Man         = 5,  // 10 игроков (героич.)
+        RaidHeroic25Man         = 6,  // 25 игроков (героич.)
+        RaidLfr25Man            = 7,  // Поиск рейда
+        DungeonChallenge5Man    = 8,  // Режим испытаний
+        Raid40Man               = 9,  // 40 игроков
+        ScenarioHeroic          = 11, // Сценарий героического уровня сложности
+        ScenarioNormal          = 12, // Сценарий обычного уровня сложности
+        RaidDynamicDiff         = 14  // Гибкий
     }
 }
