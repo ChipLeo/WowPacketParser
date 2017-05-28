3a0278c6ea0e589605f9bfef0fc511395be1efb7
ChipLeo <winleo@ua.fm>
2017-05-28 17:43:19 +0300

    Added SMSG_GUILD_BANK_QUERY_RESULTS FOR 23758

--- WowPacketParserModule.V7_2_0_23758/Parsers/GuildHandler.cs
+++ WowPacketParserModule.V7_2_0_23758/Parsers/GuildHandler.cs
@@ -14,6 +14,56 @@ namespace WowPacketParserModule.V7_2_0_23758.Parsers
                 packet.ReadInt32<AchievementId>("AchievementIDs", i);
         }
 
+        [Parser(Opcode.SMSG_GUILD_BANK_QUERY_RESULTS)]
+        public static void HandleGuildBankQueryResults(Packet packet)
+        {
+            packet.ReadUInt64("Money");
+            packet.ReadInt32("Tab");
+            packet.ReadInt32("WithdrawalsRemaining");
+
+            var tabInfoCount = packet.ReadInt32("TabInfoCount");
+            var itemInfoCount = packet.ReadInt32("ItemInfoCount");
+
+            for (int i = 0; i < tabInfoCount; i++)
+            {
+                packet.ReadInt32("TabIndex", i);
+
+                packet.ResetBitReader();
+
+                var bits1 = packet.ReadBits(7);
+                var bits69 = packet.ReadBits(9);
+
+                packet.ReadWoWString("Name", bits1, i);
+                packet.ReadWoWString("Icon", bits69, i);
+            }
+
+            for (int i = 0; i < itemInfoCount; i++)
+            {
+                packet.ReadInt32("Slot", i);
+
+                packet.ReadInt32("Count", i);
+                packet.ReadInt32("EnchantmentID", i);
+                packet.ReadInt32("Charges", i);
+                packet.ReadInt32("OnUseEnchantmentID", i);
+                var int76 = packet.ReadInt32("SocketEnchant", i);
+                packet.ReadInt32("Flags", i);
+
+                ItemHandler.ReadItemInstance(packet, i, "ItemInstance");
+
+                for (int j = 0; j < int76; j++)
+                {
+                    packet.ReadInt32("SocketIndex", i, j);
+                    packet.ReadInt32("SocketEnchantID", i, j);
+                }
+
+                packet.ResetBitReader();
+                packet.ReadBit("Locked");
+            }
+
+            packet.ResetBitReader();
+            packet.ReadBit("FullUpdate");
+        }
+
         [Parser(Opcode.SMSG_QUERY_GUILD_INFO_RESPONSE)]
         public static void HandleGuildQueryResponse(Packet packet)
         {
